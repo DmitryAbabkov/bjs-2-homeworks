f65f4fb38e6c35deed4fef40b765348c9dc3e5a5
Dmitry Ababkov <ababkov88@yandex.ru>
2023-06-16 01:37:21 +0500

    Task 1

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -2,8 +2,20 @@
 function solveEquation(a, b, c) {
   let arr = [];
   
+  const d = b**2-4*a*c;
+  
+  if (d < 0) {
+    arr = [];
+  } else if (d == 0) {
+    arr[0] = -b/(2*a);
+  } else {
+    arr[0] = (-b + Math.sqrt(d) )/(2*a);
+    arr[1] = (-b - Math.sqrt(d) )/(2*a);
+  }
+
   return arr;
 }
+
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
   
