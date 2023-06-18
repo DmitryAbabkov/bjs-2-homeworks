8df70c37ee000259aab6089d643a6eb5eec36fe2
Dmitry Ababkov <ababkov88@yandex.ru>
2023-06-18 23:05:57 +0500

    Task 1

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,7 +1,29 @@
 function getArrayParams(...arr) {
+  let min = 0;
+  let max = 0;
+  let avg = 0;
+  let sum = 0;
 
-  return { min: min, max: max, avg: avg };
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+    avg = sum / arr.length; 
+  }
+
+  function searchMaxAndMin () {
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i] > max) {
+        max = arr[i];
+      } else if (arr[i] < min) {
+        min = arr[i];
+      }
+    }
+  }
+
+    searchMaxAndMin();
+
+  return { min: min, max: max, avg: +avg.toFixed(2) };
 }
+
 
 function summElementsWorker(...arr) {
 
