4d1d8b6c96343e28a0ceb84d455c9d57c5c9b2ea
Dmitry Ababkov <ababkov88@yandex.ru>
2023-06-26 02:11:40 +0500

    Ready task 4

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,39 @@
 function Student(name, gender, age) {
-  
+	this.name = name;
+	this.gender = gender;
+	this.age = age;
+	this.marks = [];
 }
 
-Student.prototype.setSubject = function (subjectName) {
-  
-}
+let student1 = new Student("Igor", "male", 21);
+let student2 = new Student("Maksim", "male", 19);
 
-Student.prototype.addMarks = function (...marks) {
-  
-}
 
-Student.prototype.getAverage = function () {
-  
-}
+Student.prototype.setSubject = function(subjectName) {
+	this.subject = subjectName;
+};
 
-Student.prototype.exclude = function (reason) {
-  
-}
+Student.prototype.addMarks = function(...marks) {
+	if (this.marks != undefined) {
+		this.marks.push(...marks);
+	}
+};
+
+Student.prototype.getAverage = function() {
+	if (this.marks != undefined && this.marks.length != 0) {
+		const sum = this.marks.reduce((acc, item) => {
+			return acc + item;
+		}, 0);
+		const result = sum / this.marks.length;
+		return result;
+	} else {
+		return 0;
+	}
+};
+
+
+Student.prototype.exclude = function(reason) {
+	delete this.subject;
+	delete this.marks;
+	this.excluded = reason;
+};
---
+++ index.js
@@ -0,0 +1,37 @@
+function getUsersNamesInAgeRange(users, gender) {
+
+  if (users.length > 0 && (users[0].gender == "мужской" || users[0].gender == "женский") ){
+    const filtrGender = users.filter(item => gender.includes(item.gender));
+    const mapping = filtrGender.map(item => {
+      return item.age;
+   });
+    const sumAge = mapping.reduce((sum, item) => {
+      return (sum + item);
+   });
+    const result = sumAge / mapping.length;
+    console.log(users[0].gender);
+  
+  } else {
+    return 0;
+  }
+   
+}
+
+  const people = [
+    {firstName: "Александр", secondName: "Карпов", age: 17, gender: "мужской"},
+    {firstName: "Егор", secondName: "Морозов", age: 21, gender: "мужской"},
+    {firstName: "Мелисса", secondName: "Леонова", age: 40, gender: "женский"},
+    {firstName: "Мелания", secondName: "Савельева", age: 37, gender: "женский"},
+    {firstName: "Мария", secondName: "Овчинникова", age: 18, gender: "женский"},
+    {firstName: "Марьяна", secondName: "Котова", age: 17, gender: "женский"},
+    {firstName: "Фёдор", secondName: "Селезнев", age: 50, gender: "мужской"},
+    {firstName: "Георгий", secondName: "Петров", age: 35, gender: "мужской"},
+    {firstName: "Даниил", secondName: "Андреев", age: 49, gender: "мужской"},
+    {firstName: "Дарья", secondName: "Савельева", age: 25, gender: "женский"},
+    {firstName: "Михаил", secondName: "Шаров", age: 22, gender: "мужской"},
+    {firstName: "Владислав", secondName: "Давыдов", age: 40, gender: "мужской"},
+    {firstName: "Илья", secondName: "Казаков", age: 35, gender: "мужской"},
+    {firstName: "Евгений", secondName: "Кузьмин", age: 19, gender: "мужской"},
+  ];
+
+  console.log(getUsersNamesInAgeRange(people, "мужской"));
