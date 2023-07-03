83bffb2b62add8501fa303be66cfd728b4d249ae
Dmitry Ababkov <ababkov88@yandex.ru>
2023-07-04 00:39:56 +0500

    All task ready

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,57 @@
-﻿
+﻿function parseCount(number) {
+	let res = Number.parseFloat(number);
+	if (isNaN(res)) {
+		throw new Error('Невалидное значение');
+	} else {
+		return res;
+	}
+}
+
+function validateCount(number) {
+	try {
+		return parseCount(number);
+	} catch (e) {
+		return e;
+	}
+}
+
+// Task 2
+
+
+class Triangle {
+	constructor(a, b, c) {
+		this.a = a;
+		this.b = b;
+		this.c = c;
+
+		if (a + b < c || a + c < b || b + c < a) {
+			throw new Error('Треугольник с такими сторонами не существует');
+		}
+	}
+	get perimeter() {
+		return this.a + this.b + this.c;
+	}
+
+	get area() {
+		const p = 0.5 * this.perimeter;
+		return +Math.sqrt(p * (p - this.a) * (p - this.b) * (p - this.c)).toFixed(3);
+	}
+}
+
+function getTriangle(a, b, c) {
+	try {
+
+		return new Triangle(a, b, c);
+
+	} catch (error) {
+		return {
+			get perimeter() {
+				return 'Ошибка! Треугольник не существует';
+			},
+			get area() {
+				return 'Ошибка! Треугольник не существует';
+			}
+		};
+	}
+
+}
